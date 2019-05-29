e3e60d641840ae5198ad4226f13dff77a3c5fdd9
Pratik Dave <prtkdave@gmail.com>
2019-05-29 06:52:07 +0000

    login to nexus

--- vars/dockerImagebuild.groovy
+++ vars/dockerImagebuild.groovy
@@ -1,4 +1,4 @@
-def call(String[] FILE_ARRAY, String NEXUS_URL) {
+def call(String[] FILE_ARRAY, String NEXUS_REGISTRY) {
     
      echo FILE_ARRAY[0];
      for (file in FILE_ARRAY) {
@@ -9,7 +9,17 @@ def call(String[] FILE_ARRAY, String NEXUS_URL) {
       sh "docker build -f ${file} -t ${imagename[1]}:latst ."
       
       println "Tag Image "+imagename[1]
-      sh "docker tag ${imagename[1]}:latst ${NEXUS_URL}/${imagename[1]}:latst"
+      sh "docker tag ${imagename[1]}:latst ${NEXUS_REGISTRY}/${imagename[1]}:latst"
+
+      println "Login to NEXUS Registry"
+      withCredentials([usernamePassword(
+                       credentialsId: 'nexuscred', 
+                       passwordVariable: 'nexuspass', 
+                       usernameVariable: 'nexususer')]) {
+                    sh "docker login -u ${nexususer} -p ${nexuspass} ${NEXUS_REGISTRY}"
+    
+             }
+      sh "docker login -u admin -p admin123 ec2-34-229-190-97.compute-1.amazonaws.com:8123"
 
      }
 

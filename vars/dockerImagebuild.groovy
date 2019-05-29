70720f93956b3448966112ca8dbc2c8790a3c940
Pratik Dave <prtkdave@gmail.com>
2019-05-29 07:19:13 +0000

    change

--- vars/dockerImagebuild.groovy
+++ vars/dockerImagebuild.groovy
@@ -12,14 +12,7 @@ def call(String[] FILE_ARRAY, String NEXUS_REGISTRY) {
       sh "docker tag ${imagename[1]}:latst ${NEXUS_REGISTRY}/${imagename[1]}:latst"
 
       println "Login to NEXUS Registry"
-      withCredentials([usernamePassword(
-                       credentialsId: 'nexuscred', 
-                       passwordVariable: 'nexuspass', 
-                       usernameVariable: 'nexususer')]) {
-                    sh "docker login -u ${nexususer} -p ${nexuspass} ${NEXUS_REGISTRY}"
-    
-             }
-      sh "docker login -u admin -p admin123 ec2-34-229-190-97.compute-1.amazonaws.com:8123"
+       sh "docker login -u admin -p admin123 ec2-34-229-190-97.compute-1.amazonaws.com:8123"
 
      }
 

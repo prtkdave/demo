b952de8ceaf877bcfdec57bbb6000793fc8227e7
Pratik Dave <prtkdave@gmail.com>
2019-05-29 05:26:27 +0000

    build image

--- vars/dockerImagebuild.groovy
+++ vars/dockerImagebuild.groovy
@@ -3,9 +3,11 @@ def call(String[] FILE_ARRAY) {
      echo FILE_ARRAY[0];
      for (file in FILE_ARRAY) {
      
-      file=file.split("-")
-      println file[1]
-  
+      imagename=file.split("-")
+      println "Building image"+imagename[1]
+
+      sh "docker build -f file -t imagename[1]:latst ."
+   
      }
 
 

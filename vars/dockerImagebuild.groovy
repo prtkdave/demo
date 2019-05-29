369b0bed608e89a008dfd1fe8ffb4b5fdb0395db
Pratik Dave <prtkdave@gmail.com>
2019-05-29 06:36:12 +0000

    added taging image

--- vars/dockerImagebuild.groovy
+++ vars/dockerImagebuild.groovy
@@ -1,15 +1,16 @@
-def call(String[] FILE_ARRAY) {
+def call(String[] FILE_ARRAY, String NEXUS_URL) {
     
      echo FILE_ARRAY[0];
      for (file in FILE_ARRAY) {
      
       imagename=file.split("-")
-      println "Building image"+imagename[1]
-
+      
+      println "Building image "+imagename[1]
       sh "docker build -f ${file} -t ${imagename[1]}:latst ."
-   
+      
+      println "Tag Image "+imagename[1]
+      sh "docker tag ${imagename[1]}:latst ${NEXUS_URL}/${imagename[1]}:latst"
+
      }
-
-
 
 }

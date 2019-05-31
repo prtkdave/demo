3ba3277f16e00de0982216702069bc951be688bf
Pratik Dave <prtkdave@gmail.com>
2019-05-31 13:16:52 +0000

    changes to repo

--- vars/dockerRun.groovy
+++ vars/dockerRun.groovy
@@ -1,7 +1,5 @@
 def call(String DOCKER_PORT, String HOST_PORT, String IMAGE_NAME, String IMAGE_TAG, String DOCKER_NAME) {
       
-      println "Running Container "+DOCKER_NAME
+      println "Running Container "+ DOCKER_NAME
       sh "docker run -d -p ${DOCKER_PORT}:${HOST_PORT} --name ${DOCKER_NAME} ${IMAGE_NAME}:${IMAGE_TAG}"
      }
-
-}
